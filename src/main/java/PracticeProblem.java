33d40caca0f7f88149bb1defbd672a29744786ad
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-04-08 09:03:51 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,18 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+public static void main(String args[]) {
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+static int number2 = -1;
+public static int towerOfHanoi(int number) {
+		if (number < 3 && number2 == -1) {
+		return -1;
+		}
+		number2 = number2 + 1;
 
-	public static void q2() {
-		//Write question 2 code here
+		if (number == 1) {
+		return 1;
+		}
+	return 2 * towerOfHanoi(number - 1) + 1;
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
